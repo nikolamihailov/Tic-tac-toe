57eae317a2f484649df380af9c37ce307031b394
Nikola Mihaylov <nikolanm06@abv.bg>
2023-03-10 14:11:03 +0200

    Fixed popup screen with close button

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ let xScore = document.getElementById("X");
 let oScore = document.getElementById("O");
 const clearScore = document.querySelector(".clearScore");
 const winner = document.querySelector(".popup");
+const closePopup = document.getElementById("close");
+const winMessage = document.getElementById("message");
 let xScoreN = Number(xScore.innerText);
 let oScoreN = Number(oScore.innerText);
 
 clearScore.addEventListener("click", () => {
     xScore.innerText = 0;
     oScore.innerText = 0;
-})
+});
+
 newGame.addEventListener("click", newGameF);
+
+closePopup.addEventListener("click", () => {
+    winner.classList.add("hidden");
+})
 let cells = ["", "", "", "", "", "", "", "", ""];
 let start = "circle";
 function createBoard() {
@@ -31,13 +38,7 @@ let winningCombos = [[1, 2, 3], [7, 8, 9],
 [3, 6, 9],
 [1, 5, 9],
 [3, 5, 7]];
-/*let winningCombos = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2],
-[4, 5, 6], [4, 6, 5], [5, 6, 4], [5, 4, 6], [6, 5, 4], [6, 4, 5],
-[1, 4, 7], [1, 7, 4], [4, 7, 1], [4, 1, 7], [7, 1, 4], [7, 4, 1],
-[2, 5, 8], [2, 8, 5], [5, 2, 8], [5, 8, 2], [8, 2, 5], [8, 5, 2],
-[3, 6, 9], [3, 9, 6], [6, 3, 9], [6, 9, 3], [9, 6, 3], [9, 3, 6],
-[1, 5, 9], [1, 9, 5], [5, 9, 1], [5, 1, 9], [9, 5, 1], [9, 1, 5],
-[3, 5, 7], [3, 7, 5], [5, 7, 3], [5, 3, 7], [7, 5, 3], [7, 3, 5]];*/
+
 let ids = [];
 function makeShape(e) {
     const el = document.createElement("div");
@@ -49,16 +50,16 @@ function makeShape(e) {
         start = "circle";
     }
 
-
     e.target.append(el);
     checkWin(e);
     if (ids.length === 9 && !board.classList.contains("win")) {
         if (!winner.classList.contains("hidden")) {
             info.innerText = ""
         } else {
-            winner.innerHTML = "It's a draw!!! <br> Nobody wins!";
             winner.classList.remove("hidden");
-            setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
+            winMessage.innerText = "It's a draw!!!\n Nobody wins!";
+
+            //setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
         }
     } else {
         if (!winner.classList.contains("hidden")) {
@@ -82,9 +83,9 @@ function checkWin(e) {
             && idsCircle.toString().includes(`${winCombo[1].toString()}`)
             && idsCircle.toString().includes(`${winCombo[2].toString()}`)) {
             //info.innerText = "Circle wins!!!";
-            winner.innerHTML = "Circle wins!!! <br> Congrats!";
             winner.classList.remove("hidden");
-            setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
+            winMessage.innerText = "Circle wins!!! \n Congrats!";
+            //setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
             board.classList.add("win");
             oScoreN++;
             oScore.innerText = oScoreN;
@@ -94,9 +95,9 @@ function checkWin(e) {
             && idsCross.toString().includes(`${winCombo[2].toString()}`)) {
             board.classList.add("win");
             //info.innerText = "Cross wins!!!";
-            winner.innerHTML = "Cross wins!!! <br> Congrats!";
             winner.classList.remove("hidden");
-            setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
+            winMessage.innerText = "Cross wins!!!\n Congrats!";
+            //setInterval(() => { winner.innerHTML = ""; winner.classList.add("hidden") }, 3000);
             xScoreN++;
             xScore.innerText = xScoreN;
         }
@@ -111,6 +112,7 @@ const cellsElements = document.querySelectorAll(".cell");
 console.log(cellsElements);
 
 function newGameF() {
+    winner.classList.add("hidden");
     board.classList.remove("win");
     removeAllChildren(board);
     createBoard();
